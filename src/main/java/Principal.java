17bfd886ef45263bee2d466ad7e7bf2149f55436
aalvarezbretana <aalvarezbretana@danielcastelao.org>
2020-02-28 14:11:34 +0100

    Subida acceso con token

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -7,13 +7,7 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) throws IOException {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Cual es tu usuario?");
-        String usuario = sc.nextLine();
-        System.out.println("Cual es tu contraseña?");
-        String contras = sc.nextLine();
-        GitHub github = new GitHubBuilder().withPassword(usuario, contras).build();
+        GitHub github = new GitHubBuilder().withOAuthToken("5112fd8e2008ef780c7cc8b8b3d910708d862b0b").build();
         GHRepository repo = github.createRepository(
                 "PruebasAcceso", "repositorio de prueba",
                 "https://www.google.es/", false/*private*/);
