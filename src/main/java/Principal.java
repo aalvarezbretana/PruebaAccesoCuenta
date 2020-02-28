142f60714ae9e6cae9206469628c05ae31613ed0
aalvarezbretana <aalvarezbretana@danielcastelao.org>
2020-02-28 14:34:57 +0100

    Subida acceso con archivo

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) throws IOException {
-        GitHub github = new GitHubBuilder().withOAuthToken("5112fd8e2008ef780c7cc8b8b3d910708d862b0b").build();
+        GitHub github = GitHubBuilder.fromPropertyFile("/home/local/DANIELCASTELAO/aalvarezbretana/IdeaProjects/PluginNetbeans/.github").build();
         GHRepository repo = github.createRepository(
-                "PruebasAcceso", "repositorio de prueba",
+                "PruebasAccesoFile", "repositorio de prueba",
                 "https://www.google.es/", false/*private*/);
         repo.addCollaborators(github.getUser("damiancastelao"));
 
